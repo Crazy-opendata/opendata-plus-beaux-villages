edd16ab41fd4d315985f8e5ac3d3ac5b52da13e9
Wilkins <thibault@taillandier.name>
2015-09-08 23:08:04 +0200

    Export to CSV format

--- extract.php
+++ extract.php
@@ -2,7 +2,6 @@
 
 $mysql_user  = $argv[1];
 $mysql_pass  = $argv[2];
-$mysql_cmd   = "mysqldump -u$mysql_user -p$mysql_pass --extended-insert=false --skip-dump-date --skip-add-drop-table ";
 $mysql_db    = "data";
 $mysql_table = "villages";
 
@@ -14,8 +13,59 @@ create_country('Italia');
 create_country('Belgique');
 create_country('Japan');
 
+/**
+ * Generate the SQL export for a country
+ * 
+ * @param country the country name
+ * 
+ * @return void
+ */
 function create_country($country=null) {
-    global $mysql_cmd, $mysql_db, $mysql_table;
+
+    create_country_sql($country);
+    create_country_csv($country);
+}
+
+
+/**
+ * Generate the SQL export for a country
+ * 
+ * @param country the country name
+ * 
+ * @return void
+ */
+function create_country_csv($country=null) {
+    global $mysql_db, $mysql_table, $mysql_user, $mysql_pass;
+
+    $where_sql = "";
+    if ($country) {
+        $where_sql = "where country=\\\"$country\\\"";
+    }
+    if (!$country) {
+        $country = "World";
+    }
+    $filename = "villages.csv";
+    $dir = $country.'/csv';
+    if (!is_dir($dir)) {
+        mkdir($dir);
+    }
+
+    // Source http://stackoverflow.com/questions/12040816/mysqldump-in-csv-format
+    exec("/usr/bin/mysql -B -u$mysql_user -p$mysql_pass $mysql_db -e \"SELECT * FROM $mysql_table $where_sql ORDER BY village;\""
+        .' | sed "s/\'/\\\'/;s/\t/\";\"/g;s/^/\"/;s/$/\"/;s/\n//g" > '.$country.'/csv/'.$filename);
+
+}
+
+/**
+ * Generate the SQL export for a country
+ * 
+ * @param country the country name
+ * 
+ * @return void
+ */
+function create_country_sql($country=null) {
+    global $mysql_db, $mysql_table, $mysql_user, $mysql_pass;
+    $mysql_cmd   = "mysqldump -u$mysql_user -p$mysql_pass --extended-insert=false --skip-dump-date --skip-add-drop-table ";
 
     $filename = "World/sql/villages.sql";
     $where_sql = "";
