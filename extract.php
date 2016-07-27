a4368c612cb865cc7b1149d5a734a3b3571a4140
Wilkins <thibault@taillandier.name>
2016-07-27 23:49:41 +0200

    Using core tools and adding KML export

--- extract.php
+++ extract.php
@@ -1,18 +1,17 @@
 <?php
 
-namespace Villages;
+namespace CrazyOpendata;
 
-use Villages\Export\CSV;
-use Villages\Export\JSON;
-use Villages\Export\GeoJSON;
-use Villages\Export\Mysql;
+require_once __DIR__."/vendor/autoload.php";
 
-spl_autoload_register(
-    function($class) {
-        $class = str_replace("\\", "/", $class);
-        include_once "src/$class.php";
-    }
-);
+use CrazyOpendata\Core\Config;
+use CrazyOpendata\Core\Export;
+use CrazyOpendata\Core\Export\CSV;
+use CrazyOpendata\Core\Export\JSON;
+use CrazyOpendata\Core\Export\GeoJSON;
+use CrazyOpendata\Core\Export\Mysql;
+use CrazyOpendata\Core\Export\KML;
+
 
 $mysql_user  = $argv[1];
 $mysql_pass  = $argv[2];
@@ -20,16 +19,10 @@ $mysql_db    = "data";
 $mysql_host  = "localhost";
 $mysql_table = "villages";
 
-$data_dir = "data";
+define("WORLD", "World");
 
-$config = new Config(
-    $mysql_user,
-    $mysql_pass,
-    $mysql_db,
-    $mysql_host,
-    $mysql_table,
-    $data_dir
-);
+$exportName  = "villages";
+$data_dir    = "data";
 
 $countries = array(
     'France',
@@ -39,20 +32,40 @@ $countries = array(
     'Japan',
     'Canada',
     'Deutschland',
-    Config::WORLD,
+    WORLD,
 );
 
 foreach ($countries as $country) {
+    $config = new Config(
+        $mysql_user,
+        $mysql_pass,
+        $mysql_db,
+        $mysql_host,
+        $mysql_table,
+        "$data_dir/$country"
+    );
+
+    $dbh = $config->getDB();
+    $where = $country != WORLD ? " WHERE country=\"$country\"" : "";
+    $sth = $dbh->prepare("SELECT * FROM ".$config->mysqlTable.$where);
+    $sth->execute();
+    $liste = $sth->fetchAll(\PDO::FETCH_ASSOC);
+
+
     echo "\nExporting $country\n";
     $csv = new CSV($config);
-    $csv->export($country);
+    $csv->export($liste, $exportName);
 
     $json = new JSON($config);
-    $json->export($country);
+    $json->export($liste, $exportName);
 
     $geojson = new GeoJSON($config);
-    $geojson->export($country);
+    $geojson->export($liste, $exportName);
 
     $mysql = new Mysql($config);
-    $mysql->export($country);
+    $mysql->options = $country != WORLD ? "country=\"$country\"" : null;
+    $mysql->export($liste, $exportName);
+
+    $kml = new KML($config);
+    $kml->export($liste, $exportName);
 }
